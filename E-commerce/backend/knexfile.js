895521f174fe114c0493599e23004ddf2f5aa415
AleHaritos <aleharitos@gmail.com>
2022-01-13 12:28:05 -0300

    first commit

---
+++ E-commerce/backend/knexfile.js
@@ -0,0 +1,20 @@
+// Update with your config settings.
+
+module.exports = {
+
+    client: 'postgresql',
+    connection: {
+      database: 'agl',
+      user:     'postgres',
+      password: '1234'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  
+
+};
